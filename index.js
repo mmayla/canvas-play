d406beaf3d51f86450f2034d9e77f844771ba174
Mohamed Mayla <mohamed.mayla@gmail.com>
2018-05-14 21:10:57 +0200

    init

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Jimp = require('jimp');
 const wresenham = require('wresenham');
+const _ = require('lodash');
 
 const drawLine = (image, from, to, color = 0x000000FF, stroke = 1) => {
   wresenham(from, to, stroke, (x, y) => {
@@ -7,16 +8,36 @@ const drawLine = (image, from, to, color = 0x000000FF, stroke = 1) => {
   });
 }
 
-const drawTable = (image, from, width, columns, rowsCount, rowHeight, stroke, color) => {
+const drawTable = (image, from, columns, rowsCount, rowHeight, color = 0x000000FF, stroke = 1) => {
+  const height = rowHeight * rowsCount;
+  const width = _.sum(columns);
 
+  // Draw rows
+  let rowx = from[0];
+  let rowy = from[1];
+
+  for (let i = 0; i < rowsCount + 1; i+=1) {
+    drawLine(image, [rowx, rowy], [rowx + width, rowy], color, stroke);
+    rowy += rowHeight;
+  }
+
+  // Draw columns
+  let colx = from[0];
+  let coly = from[1];
+
+  for (let i = 0; i < columns.length + 1; i+=1) {
+    drawLine(image, [colx, coly], [colx, from[1] + height], color, stroke);
+    colx += columns[i];
+  }
 }
 
 const demo = () => {
   const color = Jimp.rgbaToInt(247, 134, 206, 255);
   console.log(color.toString(16));
-  const image = new Jimp(500, 1000, 0xFF0000FF);
+  const image = new Jimp(500, 1000, 0xFFFFFFFF);
 
-  drawLine(image, [0, 50], [500, 50], 0x000000FF, 3);
+  // drawLine(image, [0, 50], [500, 50], 0x000000FF, 3);
+  drawTable(image, [10, 30], [20, 100, 50], 5, 50, 0x000000FF, 1);
 
   image.write('dist/test.jpg');
 };
